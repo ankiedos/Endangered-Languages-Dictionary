c8dd16db0fe0b894be2f7b76aece0b69a83cb252
ankiedos <70840444+ankiedos@users.noreply.github.com>
2023-04-10 17:40:59 +0200

    Adapted language change for the index of indices.

--- langs.js
+++ langs.js
@@ -2,5 +2,6 @@ function change_lang()
 {
     var select = document.querySelector("select[name=langs]");
     var URL = window.location.href;
-    window.location.href = URL.substring(0, URL.lastIndexOf('.', URL.length - 6)) + "." + select.value + ".html";
+    if(URL == "https://ankiedos.github.io/Endangered-Languages-Dictionary/") window.location.href = "https://ankiedos.github.io/Endangered-Languages-Dictionary/index." + select.value + ".html";
+    else window.location.href = URL.substring(0, URL.lastIndexOf('.', URL.length - 6)) + "." + select.value + ".html";
 }
