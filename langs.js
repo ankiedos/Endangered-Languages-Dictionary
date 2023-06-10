a84ac98f2093ee55a4867ace8c663427e2cb9fd7
ankiedos <70840444+ankiedos@users.noreply.github.com>
2023-06-10 15:09:51 +0200

    Added multi-language handling of parts of speech.

--- langs.js
+++ langs.js
@@ -5,3 +5,38 @@ function change_lang()
     if(URL == "https://ankiedos.github.io/Endangered-Languages-Dictionary/") window.location.href = "https://ankiedos.github.io/Endangered-Languages-Dictionary/index." + select.value + ".html";
     else window.location.href = URL.substring(0, URL.lastIndexOf('.', URL.length - 6)) + "." + select.value + ".html";
 }
+function translatePOS(pos, lang) {
+    switch(lang) {
+        case "pl":
+            switch(pos) {
+                case "n": return "rzeczownik";
+                case "adj": return "przymiotnik";
+                case "adv": return "przysłówek";
+                case "perpron": return "zaimek osobowy";
+                case "posspron": return "zaimek dzierżawczy";
+                case "part": return "partykuła";
+                case "prep": return "przyimek";
+                case "conj": return "spójnik";
+                case "onomatopoeia": return "onomatopeja";
+                case "vt": return "czasownik przechodni";
+                case "vint": return "czasownik nieprzechodni";
+                case "participium": return "imiesłów";
+            }
+        case "en":
+            switch(pos) {
+                case "n": return "noun";
+                case "adj": return "adjective";
+                case "adv": return "adverb";
+                case "perpron": return "personal pronoun";
+                case "posspron": return "possesive pronoun";
+                case "part": return "particle";
+                case "prep": return "preposition";
+                case "conj": return "conjunction";
+                case "onomatopoeia": return "onomatopoeia";
+                case "vt": return "transitive verb";
+                case "vint": return "intransitive verb";
+                case "participium": return "participle";
+            }
+        default: return "";
+    }
+}
